7df498a59f3a17db3347e4eaf878bd735ec13f16
SHIMADA Keiki <shimada.cake@gmail.com>
2016-02-07 01:45:58 +0900

    add stylesheet

--- bin/run.sh
+++ bin/run.sh
@@ -7,3 +7,5 @@ for F in *.md; do
   pandoc -f markdown_github $F --filter bin/ymltbl.py -t html5 -o build/_html/__${F%.md}.html
   pandoc -f markdown_github $F --filter bin/ymltbl.py -t html5 -s -o build/html/${F%.md}.html
 done
+
+cp css/*.css build/html/css
