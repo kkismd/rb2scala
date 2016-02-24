2a3bd5b6af0984d017753df99356e8f33b2af9f4
SHIMADA Keiki <shimada.cake@gmail.com>
2016-02-24 22:53:18 +0900

    fix html-parts generation

--- bin/run.sh
+++ bin/run.sh
@@ -10,7 +10,7 @@ for F in *.md; do
   esac
 
   echo $F...
-  pandoc -f markdown_github $F --filter bin/ymltbl.py -t html5 -o build/_html/__${F%.md}.html
+  pandoc -f markdown_github+header_attributes $F --filter bin/ymltbl.py -t html5 -o build/_html/__${F%.md}.html
   pandoc -f markdown_github+header_attributes $F --filter bin/ymltbl.py -t html5 -s $tocopt -c css/style.css -o build/html/${F%.md}.html
 done
 
